bdfc3785bd5f4e9c27feb78ca2820857e66ba864
Ramazan Ittiev <rittiev@devexperts.com>
2022-08-07 15:42:32 +0300

    [60] switch to local mock data from airtable in promo

--- src/views/promo/promo.tsx
+++ src/views/promo/promo.tsx
@@ -1,8 +1,7 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC } from 'react';
 import Typography from '../../components/Typography';
 import { PromoLayout } from './promo-layout';
-import { airtableBase } from '../../app';
-import { mapPromoData } from '../../services/mappers';
+import { promoMock } from '../../mocks/promo.mock';
 
 interface PromoProps {
 	handleOnView: (tabId: string) => void;
@@ -10,57 +9,27 @@ interface PromoProps {
 }
 
 interface PromoDataProps {
-	alt: string;
 	image: string;
 	title: string;
 	subtitle: string;
 }
 
 export const Promo: FC<PromoProps> = ({ currentTab, handleOnView }) => {
-	const [data, setData] = useState<PromoDataProps>();
-
-	useEffect(() => {
-		if (data === undefined) {
-			airtableBase('Promo')
-				.select({
-					view: 'Grid view',
-				})
-				.eachPage(records => {
-					// @ts-ignore
-					return setData(mapPromoData(records)[0]);
-				});
-		}
-	}, [data]);
-
+	const { title, subtitle, image }: PromoDataProps = promoMock;
 	return (
 		<PromoLayout
 			currentTab={currentTab}
 			handleOnView={handleOnView}
 			sxBackground={{
-				backgroundImage: `url(${data?.image})`,
+				backgroundImage: `url(${image})`,
 				backgroundPosition: 'center',
 			}}>
 			<Typography color="inherit" align="center" component="h1" variant="h2" marked="center">
-				{data?.title}
+				{title}
 			</Typography>
 			<Typography color="inherit" align="center" component="h2" variant="h5" sx={{ mb: 4, mt: 4 }}>
-				{data?.subtitle}
+				{subtitle}
 			</Typography>
-			{/*<Button*/}
-			{/*	href="#rooms"*/}
-			{/*	sx={{*/}
-			{/*		color: '#fff',*/}
-			{/*		border: '1px solid',*/}
-			{/*		padding: '8px 16px',*/}
-			{/*		background: 'rgba(255,255,255,.2)',*/}
-			{/*		transition: 'background 0.5s',*/}
-
-			{/*		'&hover': {*/}
-			{/*			background: 'transparent',*/}
-			{/*		},*/}
-			{/*	}}>*/}
-			{/*	К номерам*/}
-			{/*</Button>*/}
 		</PromoLayout>
 	);
 };
