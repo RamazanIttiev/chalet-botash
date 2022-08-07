9e11d686b981e28d7aa61befdc55a4e9a391880b
Ramazan Ittiev <rittiev@devexperts.com>
2022-08-07 16:07:58 +0300

    [62] removed gallery skeleton

--- src/views/gallery/components/gallery.tsx
+++ src/views/gallery/components/gallery.tsx
@@ -3,7 +3,6 @@ import { Box } from '@mui/system';
 import { Grid, Modal } from '@mui/material';
 import { GalleryData } from '../galary.model';
 import { GalleryCarousel } from './gallery-carousel';
-import { GallerySkeleton } from './gallery-skeleton';
 import Typography from '../../../components/Typography';
 import { CurrentTabProps } from '../../../models/active-tab.model';
 
@@ -34,88 +33,77 @@ export const Gallery: FC<GalleryProps> = ({
 			<Typography variant="h4" align="center" component="h2" mb={8}>
 				Галерея
 			</Typography>
-			{images.length !== 0 ? (
-				screenWidth && screenWidth >= 480 ? (
-					<Grid container spacing={1}>
-						{images.map(({ id, image }, index) => (
-							<Grid item md={4} sm={4} xs={12}>
-								<ImageIconButton
-									aria-label={`Gallery_image_${id}`}
-									key={id}
-									onClick={() => showModalImage(index)}
+			{screenWidth && screenWidth >= 480 ? (
+				<Grid container spacing={1}>
+					{images.map(({ id, image }, index) => (
+						<Grid item md={4} sm={4} xs={12}>
+							<ImageIconButton
+								aria-label={`Gallery_image_${id}`}
+								key={id}
+								onClick={() => showModalImage(index)}
+								style={{
+									width: '100%',
+								}}>
+								<Box
+									sx={{
+										position: 'absolute',
+										left: 0,
+										right: 0,
+										top: 0,
+										bottom: 0,
+										backgroundSize: 'cover',
+										backgroundPosition: 'center 40%',
+										backgroundImage: `url(${image})`,
+									}}
+								/>
+								<ImageBackdrop
+									className="imageBackdrop"
 									style={{
-										width: '100%',
-									}}>
-									<Box
-										sx={{
-											position: 'absolute',
-											left: 0,
-											right: 0,
-											top: 0,
-											bottom: 0,
-											backgroundSize: 'cover',
-											backgroundPosition: 'center 40%',
-											backgroundImage: `url(${image})`,
-										}}
-									/>
-									<ImageBackdrop
-										className="imageBackdrop"
-										style={{
-											[theme.breakpoints.down('md')]: {
-												display: 'none',
+										[theme.breakpoints.down('md')]: {
+											display: 'none',
 
-												'&:last-child': {
-													display: 'block',
-												},
+											'&:last-child': {
+												display: 'block',
 											},
-										}}
-									/>
-									<Box
-										sx={{
-											position: 'absolute',
-											left: 0,
-											right: 0,
-											top: 0,
-											bottom: 0,
-											display: 'flex',
-											alignItems: 'center',
-											justifyContent: 'center',
-											color: 'common.white',
-										}}>
-										<Typography component="h3" variant="h6" color="inherit">
-											{index === 5 && '+ еще'}
-										</Typography>
-									</Box>
-								</ImageIconButton>
-							</Grid>
-						))}
-
-						<Modal
-							open={isModalOpen}
-							onClose={toggleModal}
-							sx={{
-								width: '60%',
-								height: '70%',
-								margin: '0 auto',
-								top: '15%',
-
-								[theme.breakpoints.down('md')]: {
-									width: '95%',
-								},
-							}}>
-							<GalleryCarousel images={sliderImages} currentIndex={currentIndex} />
-						</Modal>
-					</Grid>
-				) : (
-					<GalleryCarousel
-						images={sliderImages}
-						currentIndex={currentIndex}
-						height={400}
-						mt={screen.width && screen.width >= 480 ? 0 : '64px'}
-					/>
-				)
+										},
+									}}
+								/>
+								<Box
+									sx={{
+										position: 'absolute',
+										left: 0,
+										right: 0,
+										top: 0,
+										bottom: 0,
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'center',
+										color: 'common.white',
+									}}>
+									<Typography component="h3" variant="h6" color="inherit">
+										{index === 5 && '+ еще'}
+									</Typography>
+								</Box>
+							</ImageIconButton>
+						</Grid>
+					))}
+				</Grid>
 			) : (
-				<GallerySkeleton images={images} screenWidth={screenWidth} />
+				<Modal
+					open={isModalOpen}
+					onClose={toggleModal}
+					sx={{
+						width: '60%',
+						height: '70%',
+						margin: '0 auto',
+						top: '15%',
+
+						[theme.breakpoints.down('md')]: {
+							width: '95%',
+						},
+					}}>
+					<GalleryCarousel images={sliderImages} currentIndex={currentIndex} />
+				</Modal>
 			)}
 		</React.Fragment>
 	);
